1cfa266195280134505f12a3d7bad6905790ae60
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-08-30 15:08:18 +0300

    clean up

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 import platform
 
 
-class Gizmo:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __eq__(self, other):
-
-        if id(self) == id(other):
-            return True
-
-        if not isinstance(other, Gizmo):
-            return False
-
-        return self.name == other.name \
-               and self.age == other.age
-
-
 def main():
-    x = Gizmo("max", 31)
-    y = Gizmo("max", 30)
+    data = {"one": 1, "two": 2}
 
-    if x == y:
-        print("Equal")
+    for k, v in data.items():
+        print("%s=%s" % (k, v))
 
     print("Main done.. python-%s." % platform.python_version())
 
