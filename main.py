c918c0101438e4a2f2c73c0cd0791ff2ae6f76a1
Maksym Stepanenko <maksym.stepanenko@opower.com>
2017-09-01 12:10:47 +0300

    virtual environment moved to a separate host specific folder

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import platform
 
+import sys
+
 
 def main():
-    data = {"one": 1, "two": 2}
+    index = 10
 
-    for k, v in data.items():
-        print("%s=%s" % (k, v))
+    print(dir(sys))
+    print(locals())
+    print(globals())
 
     print("Main done.. python-%s." % platform.python_version())
-
 
 if __name__ == "__main__":
     main()
